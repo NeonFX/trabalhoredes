0ae73b6eed23802ac5d0fcecb0bb2a63ba867743
Cauã Migotto <caua.m1@puccampinas.edu.br>
2026-03-06 17:25:30 -0300

    Add files via upload

--- Trabalho-Redes/client.py
+++ Trabalho-Redes/client.py
@@ -4,8 +4,15 @@ import threading
 HOST = '127.0.0.1'
 PORT = 5000
 
+def mostrar_comandos(): 
+    print("\n===== COMANDOS DISPONÍVEIS =====")
+    print(":accept  -> aceita a corrida atual")
+    print(":cancel  -> cancela a corrida aceita")
+    print(":status  -> mostra o status do motorista")
+    print(":quit    -> encerra o programa")
+    print("=================================\n")
 
-def recebe_mensagem(sock):
+def recebe_mensagem(sock): 
     while True:
         try:
             msg = sock.recv(1024).decode()
@@ -15,27 +22,20 @@ def recebe_mensagem(sock):
         except:
             break
 
-
 def enviar_comando(sock):
     while True:
         cmd = input()
-
         sock.send(cmd.encode())
-
         if cmd == ":quit":
             break
-
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
-
     threading.Thread(target=recebe_mensagem, args=(client,), daemon=True).start()
-
+    mostrar_comandos()
     enviar_comando(client)
-
     client.close()
-
 
 if __name__ == "__main__":
     main()
