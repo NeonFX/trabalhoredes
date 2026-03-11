78a91c7e6fdf75ec2db7eafc5974e0265e9978c4
Gabbriel Nagano <gabbrielnagano@gmail.com>
2026-03-10 22:17:47 -0300

    Update server.py
    Refactor server code to improve readability and maintainability.

--- Trabalho-Redes/server.py
+++ Trabalho-Redes/server.py
@@ -4,34 +4,37 @@ import random
 import time
 from datetime import datetime
 
-HOST = '127.0.0.1'
-PORT = 5000
+#define onde o servidor irá rodar 
+HOST = '127.0.0.1' 
+PORT = 5000 
 
-corrida_atual = None
+#váriaveis que controlam o estado da corrida e a fila de motoristas
+corrida_atual = None 
 corrida_aceita = False
 fila_motoristas = []
-lock = threading.Lock()
+lock = threading.Lock() #usado para evitar condições de corrida ao acessar as variáveis compartilhadas entre threads
 
 
+#retorna a hora atual formatada para exibir nas mensagens
 def timestamp():
     return datetime.now().strftime("%H:%M:%S")
 
 def finalizar_corrida(conn):
     global corrida_aceita, corrida_atual
-    time.sleep(random.randint(10,20))
-    with lock:
+    time.sleep(random.randint(10,20))  #simula uma duração aleatória da corrida entre 10 e 20 segundos
+    with lock: #atualiza o estado da corrida para finalizada
         corrida_aceita = False
         corrida_atual = None
         conn.send(f"{timestamp()} Corrida finalizada!\n".encode())
 
-def acoes_comandos(conn, addr):
-    global corrida_aceita, corrida_atual
-    while True:
+def acoes_comandos(conn, addr): #processa os comandos enviados pelo motorista
+    global corrida_aceita, corrida_atual #variáveis globais que controlam o estado da corrida
+    while True: #loop 
         try:
-            data = conn.recv(1024).decode().strip()
+            data = conn.recv(1024).decode().strip() #servidor recebe os dados enviados pelo cliente
             if not data:
                 break
-            if data == ":accept":
+            if data == ":accept": #serve para aceitar a corrida atual
                 with lock:
                     if corrida_atual and not corrida_aceita:
                         corrida_aceita = True
@@ -40,7 +43,7 @@ def acoes_comandos(conn, addr):
                         threading.Thread(target=finalizar_corrida, args=(conn,), daemon=True).start()
                     else:
                         conn.send(f"{timestamp()} Nenhuma corrida disponível.\n".encode())
-            elif data == ":cancel":
+            elif data == ":cancel": #serve para cancelar a corrida atual
                 with lock:
                     if corrida_aceita:
                         corrida_aceita = False
@@ -49,7 +52,7 @@ def acoes_comandos(conn, addr):
                         conn.send(f"{timestamp()} Corrida cancelada.\n".encode())
                     else:
                         conn.send(f"{timestamp()} Você não está em corrida.\n".encode())
-            elif data == ":status":
+            elif data == ":status": #verifica o status do motorista, se ele está livre ou não, e sua posição na fila
                 with lock:
                     if corrida_aceita:
                         estado = "Em corrida"
@@ -74,13 +77,13 @@ def acoes_comandos(conn, addr):
 def gerador_corrida(conn):
     global corrida_atual, corrida_aceita
     while True:
-        time.sleep(random.randint(8, 15))
+        time.sleep(random.randint(8, 15)) #gera uma nova corrida num intervalo aleatório entre 8 e 15 segundos
         with lock:
             if corrida_aceita:
                 continue
-            distancia_passageiro = round(random.uniform(0.5, 5.0), 1)
-            viagem = round(random.uniform(1.0, 10.0), 1)
-            preco = round(viagem * random.uniform(2.5, 4.0), 2)
+            distancia_passageiro = round(random.uniform(0.5, 5.0), 1) #distância do passageiro até o motorista
+            viagem = round(random.uniform(1.0, 10.0), 1) #distância da corrida
+            preco = round(viagem * random.uniform(2.5, 4.0), 2) #calcula o preço da corrida baseado na distância 
             corrida_atual = (distancia_passageiro, viagem, preco)
             msg = f"""            
 NOVA CORRIDA!!!
@@ -123,22 +126,22 @@ Digite :accept para aceitar
                         break
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    server.bind((HOST, PORT))
-    server.listen()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket do servidor usando IPv4 e TCP
+    server.bind((HOST, PORT)) #associa o socket a um endereço IP e porta definidos
+    server.listen() #servidor começa a aguardar conexões de clientes
     print("Servidor iniciado...")
 
-    conn, addr = server.accept()
+    conn, addr = server.accept() #servidor aceita a conexão do cliente 
     print(f"Motorista conectado: {addr}")
     with lock:
         fila_motoristas.append(addr)
 
     conn.send(f"{timestamp()}: CONECTADO!!\n".encode())
-    threading.Thread(target=acoes_comandos, args=(conn, addr), daemon=True).start()
+    threading.Thread(target=acoes_comandos, args=(conn, addr), daemon=True).start() #cria a thread encarregada de processar as solicitações do cliente
     threading.Thread(target=gerador_corrida, args=(conn,), daemon=True).start()
     while True:
         time.sleep(1)
-
+# "daemon = true" indica que as threads são secundárias e encerram automaticamente quando o programa principal é finalizado.
 
 if __name__ == "__main__":
     main()
