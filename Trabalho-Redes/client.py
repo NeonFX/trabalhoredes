c70e9bf6c98e66b2c7d2f94f1ba6a7fa7760a4e6
Gabbriel Nagano <gabbrielnagano@gmail.com>
2026-03-10 22:17:04 -0300

    Update client.py
    Added comments to explain the code functionality.

--- Trabalho-Redes/client.py
+++ Trabalho-Redes/client.py
@@ -4,6 +4,7 @@ import threading
 HOST = '127.0.0.1'
 PORT = 5000
 
+#exibe os comandos disponíveis para o motorista
 def mostrar_comandos(): 
     print("\n===== COMANDOS DISPONÍVEIS =====")
     print(":accept  -> aceita a corrida atual")
@@ -12,21 +13,22 @@ def mostrar_comandos():
     print(":quit    -> encerra o programa")
     print("=================================\n")
 
+#função responsável por receber mensagens do servidor e exibi-las
 def recebe_mensagem(sock): 
     while True:
         try:
-            msg = sock.recv(1024).decode()
+            msg = sock.recv(1024).decode() #cliente recebe mensagens do servidor em até 1024 bytes e converte para string
             if not msg:
                 break
             print(msg)
         except:
-            break
+            break #fecha a conexão se nehuma mensagem for recebida ou se ocorrer um erro
 
 def enviar_comando(sock):
     while True:
-        cmd = input()
+        cmd = input() #lê o comando digitado pelo motorista
         try:
-            sock.send(cmd.encode())
+            sock.send(cmd.encode()) #envia o comando para o servidor, convertendo a string para bytes
         except:
             print("Servidor desconectado")
             break
@@ -34,12 +36,12 @@ def enviar_comando(sock):
             break
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    threading.Thread(target=recebe_mensagem, args=(client,), daemon=True).start()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket do cliente usando IPv4 e TCP
+    client.connect((HOST, PORT)) #estabelece conexão com o servidor no endereço e porta definidos
+    threading.Thread(target=recebe_mensagem, args=(client,), daemon=True).start() #cria uma thread separada para receber mensagens do servidor
     mostrar_comandos()
     enviar_comando(client)
-    client.close()
+    client.close() #encerra a conexão com o servidor 
 
 if __name__ == "__main__":
     main()
